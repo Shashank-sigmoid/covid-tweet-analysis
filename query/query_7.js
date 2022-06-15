c237eead1f304fddd23a2df07602c72bdc88fe57
Shashank-sigmoid <shashank1@sigmoidanalytics.com>
2022-06-15 10:36:29 +0530

    Field name rectified

--- query/query_7.js
+++ query/query_7.js
@@ -1,10 +1,11 @@
 // Ranking of impacted countries over the last 2 months
 
 
-db.cases_data.aggregate([{$project:{"location":"$location", "RankOfImpactedCountries":1,"confirmedCases":"$confirmed", _id:0}},{
-$setWindowFields: {
-sortBy: { confirmed: -1 },
-output: {
-RankOfImpactedCountries: {
-$rank: {}}}}
-}])
+db.cases_data.aggregate([
+{ $project: { "location": "$Country", "RankOfImpactedCountries": 1, "confirmedCases": "$Confirmed cases", _id: 0 } },
+{ $setWindowFields:
+{ sortBy: { confirmed: -1 },
+output: { RankOfImpactedCountries:
+{ $rank: {}}}}}
+])
+
